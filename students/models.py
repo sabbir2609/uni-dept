76a6fce22b0a04776c07ceda9a9f10077aae90f5
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-03-02 15:45:08 +0600

    model modified

--- students/models.py
+++ students/models.py
@@ -1,3 +1,21 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    GENDER_CHOICES = [
+        ("M", "Male"),
+        ("F", "Female"),
+
+    ]
+
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    nid = models.IntegerField(max_length=16)
+    phone = models.IntegerField(max_length=11)
+    birthdate = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+    def __str__(self):
+        return self.name
