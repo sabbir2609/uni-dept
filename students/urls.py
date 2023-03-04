a79436421970a6ba8cbb20c5558d136d3aa57488
sabbir2609 <sabbir@sabbir-pc>
2023-03-04 17:42:16 +0600

    template addedbash: line 1: y: command not found template addedbash: line 1: push: command not found Template Added

--- students/urls.py
+++ students/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from students.views import StudentViewSet
+from students.views import StudentViewSet, add_students
 
 urlpatterns = [
     path('', StudentViewSet.as_view(), name='students'),
+    # path('enlist/', StudentEnlistmentViewSet.as_view(), name='enlist'),
+    path('add/', add_students, name='add'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
+
+from django.shortcuts import render
 
 from students.models import User
 
@@ -7,3 +9,20 @@ class StudentViewSet(ListView):
     model = User
     template_name = "students/index.html"
     context_object_name = "students"
+
+# class StudentEnlistmentViewSet(TemplateView):
+#     template_name = "students/add_students.html"
+
+
+def add_students(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        address = request.POST.get("address")
+        nid = request.POST.get("nid")
+        phone = request.POST.get("phone")
+        birthdate = request.POST.get("birthdate")
+        gender = request.POST.get("gender")
+
+        print( name, address, nid,phone, birthdate,gender)
+
+    return render(request, "students/add_students.html")
