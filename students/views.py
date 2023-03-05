ae4a218cc686315d0e46310f957f430c6b0781eb
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-03-05 16:26:11 +0600

    new field addes

--- students/views.py
+++ students/views.py
@@ -2,7 +2,7 @@ from django.views.generic import ListView, TemplateView
 
 from django.shortcuts import render
 
-from students.models import User
+from students.models import User, Course
 
 
 class StudentViewSet(ListView):
@@ -23,6 +23,21 @@ def add_students(request):
         birthdate = request.POST.get("birthdate")
         gender = request.POST.get("gender")
 
+        new_student = User(name=name, address=address, nid=nid, phone=phone, birthdate=birthdate, gender=gender )
+        new_student.save()
+
         print( name, address, nid,phone, birthdate,gender)
 
     return render(request, "students/add_students.html")
+
+def add_courses(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        code = request.POST.get("code")
+        description = request.POST.get("description")
+
+        new_course = Course(name=name, code=code, description=description)
+        new_course.save()
+
+        print( name, code, description)
+    return render(request, "students/add_courses.html")
