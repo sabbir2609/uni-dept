268d281d0a042e9ed56057457817beb2714cb6fe
Sabbir Hasan Munna <sabbirhasan2999@gmail.com>
2023-03-06 01:25:45 +0600

    home app and page and nav config

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from students.views import StudentViewSet, add_students, add_courses
 
 urlpatterns = [
-    path('', StudentViewSet.as_view(), name='students'),
+    path('students/', StudentViewSet.as_view(), name='students'),
     path('add_students/', add_students, name='add'),
     path('add_courses/', add_courses, name='add'),
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,5 @@
-from django.views.generic import ListView, TemplateView
-
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from students.models import User, Course
 
